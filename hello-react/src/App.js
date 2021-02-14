d86b23f6f1aaf4bd2e86b8667896672886c03735
linternas <kmg20000215@gmail.com>
2021-02-15 02:59:07 +0900

    JSX 연습

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, Fragment } from 'react';
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    const text = '당신은 어썸한가요?';
+    const conditional = true;
+    const style = {
+      backgroundColor: 'gray',
+      border: '1px solid black',
+      height: Math.round(Math.random() * 300) + 50,
+      width: Math.round(Math.random() * 300) + 50,
+      WebkitTransition: 'all',
+      MozTransition: 'all',
+      msTransition: 'all'
+    }
+
+    return (
+      <div class="my-div">
+        { /** 요소 밖에서 주석 작성 */}
+        <h1>리액트 안녕!</h1>
+        <h2>{text}</h2>
+        {
+          conditional && '보여주세요'
+        }
+        <div style={style}
+        // self-closed 태그에서만 작동하는 주석
+        // 마지막 />가 꼭 새 졸에 있어야 함.
+        /** 이렇게도 작성 가능 */
+        />
+        // 여기서 쓰면 렌더링됨
+        /** 주석 불가능 */
+      </div>
+    );
+  }
 }
 
 export default App;
