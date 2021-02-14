41bebf3deb475236822da216e4d661ed2766e1ec
linternas <kmg20000215@gmail.com>
2021-02-15 03:22:49 +0900

    4장 Event Handling

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
-import MyComponent from './MyComponent'
+// import MyComponent from './MyComponent'
+import EventPractice from './EventPractice'
 import './App.css'
 
 class App extends Component {
   render() {
     return (
-      <MyComponent name="React" age={4} />
+      // <MyComponent name="React" age={4} />
+      <EventPractice />
     );
   }
 }
