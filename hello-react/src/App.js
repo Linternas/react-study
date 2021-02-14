24ee1cd061bc8a57268a84fb2ec1f3764f2a3d79
linternas <kmg20000215@gmail.com>
2021-02-15 03:34:49 +0900

    ref 연습

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
 // import MyComponent from './MyComponent'
-import EventPractice from './EventPractice'
-import './App.css'
+// import EventPractice from './EventPractice'
+// import ValidationSample from './ValidationSample'
+import ScrollBox from './ScrollBox';
+// import './App.css'
 
 class App extends Component {
   render() {
     return (
       // <MyComponent name="React" age={4} />
-      <EventPractice />
+      // <EventPractice />
+      // <ValidationSample />
+      <div>
+        <ScrollBox ref={(ref) => this.scrollBox = ref}/>
+        <button onClick={() => this.scrollBox.scrollToBottom()}>
+          맨아래로
+        </button>
+      </div>
     );
   }
 }
