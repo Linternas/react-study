b99a972a4bbe4d098a8bdefd28145995406f748c
linternas <kmg20000215@gmail.com>
2021-02-15 03:45:27 +0900

    6장 배열

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 // import MyComponent from './MyComponent'
 // import EventPractice from './EventPractice'
 // import ValidationSample from './ValidationSample'
-import ScrollBox from './ScrollBox';
+// import ScrollBox from './ScrollBox';
+import IterationSample from './IterationSample'
 // import './App.css'
 
 class App extends Component {
@@ -11,12 +12,14 @@ class App extends Component {
       // <MyComponent name="React" age={4} />
       // <EventPractice />
       // <ValidationSample />
-      <div>
-        <ScrollBox ref={(ref) => this.scrollBox = ref}/>
-        <button onClick={() => this.scrollBox.scrollToBottom()}>
-          맨아래로
-        </button>
-      </div>
+      // <div>
+      //   <ScrollBox ref={(ref) => this.scrollBox = ref}/>
+      //   <button onClick={() => this.scrollBox.scrollToBottom()}>
+      //     맨아래로
+      //   </button>
+      // </div>
+
+      <IterationSample />
     );
   }
 }
